bb2ccdcdd9ae869c192e51fae742f641cfe5a3dd
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-09-19 12:24:30 +0200

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <React.StrictMode>
+      <div className="">
+         <header>
+            <h1>Coucou</h1>
+         </header>
+         <main>
+            <p>Salut</p>
+            <p>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet debitis deleniti
+               eveniet in iure maxime odio optio quae unde ut.
+            </p>
+         </main>
+      </div>
+   </React.StrictMode>
 )
