6c4fa14bac1335990043eb0c8e22532a23ecf984
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-10 15:49:05 +0200

    Added Auth and Login tabs for switching between them. Added hooks for handeling form, user feedback and HTTP request. ISSUE: cannot log nor create user getting error response

--- src/store/store.auth.ts
+++ src/store/store.auth.ts
@@ -19,31 +19,55 @@ type AuthActions = {
 const useAuthStore = create<AuthState & AuthActions>(set => ({
    isLoggedIn: false,
    login: async (userLogin: Login) => {
-      const postReq = await ky.post(ROUTES.api.login, {json: userLogin});
-      const res: LoginRequestSuccess | LoginRequestError = await postReq.json();
-      console.log(res);
-      if (!res.token) {
-         return {response: res.message, error: true};
-      } else {
-         set({isLoggedIn: true});
-         localStorage.setItem('token', res.token!);
-         history.pushState('', '', ROUTES.index);
-         return {response: res.message, error: false};
+      // console.log('from store auth', userLogin);
+      try {
+         const response = await ky.post(ROUTES.api.login, {json: userLogin});
+         // console.log(response);
+
+         if (!response.ok) {
+            throw new Error(response.statusText);
+         }
+         const json: LoginRequestSuccess | LoginRequestError = await response.json();
+         // console.log(response);
+         if (!json.token) {
+            throw new Error(response.statusText);
+         } else {
+            set({isLoggedIn: true});
+            localStorage.setItem('token', json.token!);
+            // history.pushState('', '', ROUTES.index);
+            return {response: json.message, error: false};
+         }
+      } catch (error: Error) {
+         return {response: 'Error : ' + String(error.message), error: true};
+
       }
+
    },
    logout: () => set({isLoggedIn: false}),
    getToken: () => {
       return localStorage.getItem('token');
    },
    signup: async (userSignup: Signup) => {
-      const postReq = await ky.post(ROUTES.api.signup, {json: userSignup});
-      const res: LoginRequestSuccess | LoginRequestError = await postReq.json();
-      console.log(res);
-      if (!res.token) {
-         return {response: res.message, error: true};
-      } else {
-         history.pushState('', '', ROUTES.auth);
-         return {response: res.message, error: false};
+      try {
+         const response = await ky.post(ROUTES.api.login, {json: userSignup});
+         // console.log(response);
+
+         if (!response.ok) {
+            throw new Error(response.statusText);
+         }
+         const json: LoginRequestSuccess | LoginRequestError = await response.json();
+         // console.log(response);
+         if (!json.token) {
+            throw new Error(response.statusText);
+         } else {
+            set({isLoggedIn: true});
+            localStorage.setItem('token', json.token!);
+            // history.pushState('', '', ROUTES.index);
+            return {response: json.message, error: false};
+         }
+      } catch (error: Error) {
+         return {response: 'Error : ' + String(error.message), error: true};
+
       }
    },
 }));
