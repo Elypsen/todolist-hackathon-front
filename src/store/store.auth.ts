1855a750623f360f27a3943239a2e9687edb13ec
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-10 14:50:37 +0200

    Add the auth and login logic to the store.auth.ts

--- src/store/store.auth.ts
+++ src/store/store.auth.ts
@@ -1,22 +1,51 @@
 // authStore.ts
-import {create} from 'zustand'
+import {create} from 'zustand';
+import ky from 'ky';
+import {Login, LoginRequestError, LoginRequestSuccess, Signup} from '../types.shared.ts';
+import {ROUTES} from '../routes.constants.ts';
 
 type AuthState = {
    isLoggedIn: boolean
 }
 
 type AuthActions = {
-   login: () => void
+   login: (userLogin: Login) => Promise<{response: string, error: boolean}>
    logout: () => void
+   getToken: () => string | null
+
+   signup: (userSignup: Signup) => Promise<{response: string, error: boolean}>
 }
 
 const useAuthStore = create<AuthState & AuthActions>(set => ({
    isLoggedIn: false,
-   login: () => {
-      set({isLoggedIn: true})
-      history.pushState('','','/')
+   login: async (userLogin: Login) => {
+      const postReq = await ky.post(ROUTES.api.login, {json: userLogin});
+      const res: LoginRequestSuccess | LoginRequestError = await postReq.json();
+      console.log(res);
+      if (!res.token) {
+         return {response: res.message, error: true};
+      } else {
+         set({isLoggedIn: true});
+         localStorage.setItem('token', res.token!);
+         history.pushState('', '', ROUTES.index);
+         return {response: res.message, error: false};
+      }
    },
    logout: () => set({isLoggedIn: false}),
-}))
+   getToken: () => {
+      return localStorage.getItem('token');
+   },
+   signup: async (userSignup: Signup) => {
+      const postReq = await ky.post(ROUTES.api.signup, {json: userSignup});
+      const res: LoginRequestSuccess | LoginRequestError = await postReq.json();
+      console.log(res);
+      if (!res.token) {
+         return {response: res.message, error: true};
+      } else {
+         history.pushState('', '', ROUTES.auth);
+         return {response: res.message, error: false};
+      }
+   },
+}));
 
-export default useAuthStore
+export default useAuthStore;
